d8e4ecaa350f2c95b032b5aacebaf1bddd514deb
Arash <arash@probe.dk>
2025-03-12 12:05:04 +0100

    style the active link in the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,27 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
+import classNames from "classnames";
 
 interface iNavProps {
   title: string;
   href: string;
 }
 
+//---------------- main component ----------------
 const NavBar = () => {
   const navLinks: iNavProps[] = [
     { title: "Dashboard", href: "/" },
     { title: "Issues", href: "/issues" },
   ];
 
+  const currentPath = usePathname();
+
+  console.log("currentPath: ", currentPath);
+
+  //---------------- JSX ----------------
   return (
     <nav className="flex items-center space-x-6 border-b px-4 h-12 mb-8">
       <Link href="/">
@@ -22,7 +32,16 @@ const NavBar = () => {
           <li key={nav.href}>
             <Link
               href={nav.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              // Active link style
+              //   className={`${
+              //     nav.href === currentPath ? "text-blue-600" : "text-zinc-600"
+              //   } hover:text-blue-400 transition-colors`}
+
+              className={classNames({
+                "text-blue-600": nav.href === currentPath,
+                "text-zinc-600": nav.href !== currentPath,
+                "hover:text-blue-400 transition-colors": true,
+              })}
             >
               {nav.title}
             </Link>
