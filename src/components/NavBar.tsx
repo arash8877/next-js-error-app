d61931e4ebc1ac5f3f22fb3f78c9a652917c7529
Arash <arash@probe.dk>
2025-03-12 11:09:04 +0100

    build the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,33 @@
 import Link from "next/link";
+import { FaBug } from "react-icons/fa";
+
+interface iNavProps {
+  title: string;
+  href: string;
+}
 
 const NavBar = () => {
+  const navLinks: iNavProps[] = [
+    { title: "Dashboard", href: "/" },
+    { title: "Issues", href: "/issues" },
+  ];
+
   return (
     <nav className="flex items-center space-x-6 border-b px-4 h-12 mb-8">
-      <Link href="/">Logo</Link>
+      <Link href="/">
+        <FaBug />
+      </Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+        {navLinks.map((nav) => (
+          <li key={nav.href}>
+            <Link
+              href={nav.href}
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            >
+              {nav.title}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
