07da8c34e46081fff895476d25d0c5efe276eda1
Arash <arash@probe.dk>
2025-03-13 13:53:43 +0100

    add a markdown editor

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,14 +1,29 @@
-import { TextField, TextArea, Button } from '@radix-ui/themes'
-import React from 'react'
+"use client";
 
+import { useEffect, useState } from "react";
+import { TextField, Button } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+// Dynamic import with SSR disabled
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+//------------------------- NewIssuePage -------------------------
 const NewIssuePage = () => {
-  return (
-    <div className='max-w-2xl space-y-4'>
-        <TextField.Root placeholder="Title" radius='medium' />
-        <TextArea placeholder="Description" radius='medium'/>
-        <Button>Submit New Issue</Button>
-    </div>
-  )
-}
+  const [isClient, setIsClient] = useState(false);
 
-export default NewIssuePage
+  // Ensure this only renders on the client side
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  return (
+    <div className="max-w-2xl space-y-4">
+      <TextField.Root placeholder="Title" radius="medium" />
+      {isClient && <SimpleMDE placeholder="Description" />}
+      <Button>Submit New Issue</Button>
+    </div>
+  );
+};
+
+export default NewIssuePage;
